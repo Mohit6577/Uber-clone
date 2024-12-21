72c176f0cf229fdd3a07a287a183cd09ba445cb3
Mohit6577 <Mohitchoudhary657@gmail.com>
2024-12-21 21:30:06 +0530

    user authentication and profile done

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,14 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./db/db');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.route');
 
 connectDB();
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model.js');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+const blackList = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -20,4 +21,38 @@ module.exports.registerUser = async (req, res, next) => {
   });
   const token = user.generateAuthToken();
   res.status(201).json({ user, token });
+};
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select('+password');
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+  const isValid = await user.comparePassword(password);
+  if (!isValid) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+  const token = user.generateAuthToken();
+  res.cookie('token', token, { httpOnly: true });
+  res.status(200).json({ user, token });
+};
+
+module.exports.getUserProfile = async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({ user });
+};
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie('token');
+  const user = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+  await blackList.create({ token: user });
+
+  res.status(200).json({ message: 'Logged out' });
 };
